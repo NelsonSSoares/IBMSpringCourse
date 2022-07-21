934e22293967d2555231fe4deeb534eb40ed238c
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2022-07-21 16:42:04 -0300

    Dependency check

---
+++ springcore/src/main/java/com/nelson/spring/springcore/dependencycheck/Test.java
@@ -0,0 +1,20 @@
+package com.nelson.spring.springcore.dependencycheck;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/nelson/spring/springcore/dependencycheck/config.xml");
+		Prescription prescription = (Prescription) context.getBean("prescription");
+		System.out.println(prescription);
+		
+		
+	
+	}
+
+}
