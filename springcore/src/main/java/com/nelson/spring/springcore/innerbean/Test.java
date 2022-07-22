7fa148e98be3f0ca9537f56facb912b3a32d3af2
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2022-07-22 12:46:57 -0300

    DependencyCheck and innerBeans

---
+++ springcore/src/main/java/com/nelson/spring/springcore/innerbean/Test.java
@@ -0,0 +1,21 @@
+package com.nelson.spring.springcore.innerbean;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/nelson/spring/springcore/innerbean/config.xml");
+		Employee employee = (Employee) context.getBean("employee");
+
+		System.out.println(employee.hashCode());
+		
+		Employee employee1 = (Employee) context.getBean("employee");
+
+		System.out.println(employee1.hashCode());
+	}
+
+}
