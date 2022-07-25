564e6449c91fe222ad3b1bf0683e1e0591a2b82d
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2022-07-25 12:17:40 -0300

    Constructor Injection

---
+++ springcore/src/main/java/com/nelson/spring/springcore/constructorinjection/ambiguity/Addition.java
@@ -0,0 +1,22 @@
+package com.nelson.spring.springcore.constructorinjection.ambiguity;
+
+public class Addition {
+	
+	
+	public Addition(int a, double b) {
+		System.out.println("Inside the constructor");
+	}
+//	
+//	public Addition(double a, double b) {
+//		System.out.println("Inside constructor double");
+//	}
+//
+//	public Addition(int a, int b) {
+//		System.out.println("Inside constructor int");
+//	}
+//
+//	public Addition(String a, String b) {
+//		System.out.println("Inside constructor String");
+//	}
+
+}
