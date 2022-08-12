3d48ddf927ae79ab24feb89a9370e294252bf0fc
Nelson de Sousa <73802941+NelsonSSoares@users.noreply.github.com>
2022-08-12 11:08:04 -0300

    ModelMap

--- springmvc/src/main/java/com/nelson/spring/springmvc/controller/UserController.java
+++ springmvc/src/main/java/com/nelson/spring/springmvc/controller/UserController.java
@@ -1,31 +1,37 @@
 package com.nelson.spring.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.nelson.spring.springmvc.dto.User;
 
 @Controller
 public class UserController {
 	@RequestMapping("/registrationPage")
-	public ModelAndView showRegistrationPage() {
-		ModelAndView modelAndView = new ModelAndView();
+	public String showRegistrationPage() {
 
-		modelAndView.setViewName("userReg");
-
-		return modelAndView;
+		return "userReg";
+		
+		
+//		ModelAndView modelAndView = new ModelAndView();
+//
+//		modelAndView.setViewName("userReg");
+//
+//		return modelAndView;
 	}
 
 	@RequestMapping(value = "registerUser", method=RequestMethod.POST)
-	public ModelAndView registerUser(@ModelAttribute("user") User user) {
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
+	
 		System.out.println(user);
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("regResult");
-
-		return modelAndView;
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.addObject("user", user);
+//		modelAndView.setViewName("regResult");
+		model.addAttribute("user", user);
+		return "regResult";
+		//return modelAndView;
 	}
 }
